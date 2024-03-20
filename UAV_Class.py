3e691f37fbfa470f7c1aa63af2a2090b61779cfa
Noah Fabrick <nfabrick157@gmail.com>
2024-03-20 00:28:58 -0700

    idk

--- UAV_Class.py
+++ UAV_Class.py
@@ -9,11 +9,24 @@ class Environment:
         self.w = width
         self.xRange = [-(width/2),(width/2)]
         self.yRange = [-(height/2),(height/2)]
+        self.targets = []
+
+    def generateTarget(self, numTargets):
+        for i in range(numTargets):
+            self.targets.append((np.random.uniform(self.xRange[0],self.xRange[1]),np.random.uniform(self.yRange[0],self.yRange[1])))
+        return self
+    
+    def initUAVs(self, numUAVs):
+        pass
 
 
 
-testEnv = Environment(40,20)
+testEnv = Environment(100,200)
+testEnv.generateTarget(3)
+print(testEnv.targets)
 plt.plot(0,0,'r.')
+for t in testEnv.targets:
+    plt.plot(t[0],t[1],'b*')
 plt.gca().set_xlim(testEnv.xRange[0], testEnv.xRange[1])
 plt.gca().set_ylim(testEnv.yRange[0], testEnv.yRange[1])
 plt.gca().grid(True)
