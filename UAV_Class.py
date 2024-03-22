24c36add3a01dd3ffb920defbc0591ddf390964c
Noah Fabrick <nfabrick157@gmail.com>
2024-03-21 20:35:18 -0700

    kinda works; solution a little hardcoded into it

--- UAV_Class.py
+++ UAV_Class.py
@@ -1,6 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+class UAV:
+    def __init__(self,x,v,w,phi_p,phi_g):
+        self.x = x
+        self.v = v
+        self.w = w
+        self.p = [0,0] # initial best postion of UAV is home position
+        self.phi_p = phi_p
+        self.phi_g = phi_g
+
 class Environment:
 # Environment represents the scanable area where UAVs will search for target
 # Given height and width, will create a corresponding x and y range centered at the origin
@@ -9,25 +18,58 @@ class Environment:
         self.w = width
         self.xRange = [-(width/2),(width/2)]
         self.yRange = [-(height/2),(height/2)]
-        self.targets = []
-
-    def generateTarget(self, numTargets):
-        for i in range(numTargets):
-            self.targets.append((np.random.uniform(self.xRange[0],self.xRange[1]),np.random.uniform(self.yRange[0],self.yRange[1])))
-        return self
+        self.target = [np.random.uniform(self.xRange[0],self.xRange[1]),np.random.uniform(self.yRange[0],self.yRange[1])]
+        self.g = [np.random.uniform(self.xRange[0],self.xRange[1]),np.random.uniform(self.yRange[0],self.yRange[1])] 
+        self.UAVs = []
     
     def initUAVs(self, numUAVs):
-        pass
+        for i in range(numUAVs):
+            angle = ((2*np.pi)/numUAVs)*i
+            v_i = [np.cos(angle),np.sin(angle)]
+            self.UAVs.append(UAV([0,0],v_i,0.8,0.2,0.2))
 
+    def updatePos(self):
+        for uav in self.UAVs:
+            for i in range(len(uav.x)):
+                uav.x[i] += uav.v[i]
+        return self
 
+    def updateVel(self):
+        for uav in self.UAVs:
+            rP = np.random.uniform(0,1)
+            rG = np.random.uniform(0,1)
+            for i in range(len(uav.v)):
+                uav.v[i] = uav.w*uav.v[i] + uav.phi_p*rP*(uav.p[i]-uav.x[i]) + uav.phi_g*rG*(self.g[i]-uav.x[i]) 
+        return self
 
-testEnv = Environment(100,200)
-testEnv.generateTarget(3)
-print(testEnv.targets)
-plt.plot(0,0,'r.')
-for t in testEnv.targets:
-    plt.plot(t[0],t[1],'b*')
-plt.gca().set_xlim(testEnv.xRange[0], testEnv.xRange[1])
-plt.gca().set_ylim(testEnv.yRange[0], testEnv.yRange[1])
-plt.gca().grid(True)
-plt.show()
+    def updateValue(self):
+        for uav in self.UAVs:
+            if dist(uav.x,self.target) < dist(uav.p,self.target):
+                uav.p = uav.x
+            if dist(uav.p,self.target) < dist(self.g,self.target):
+                self.g = uav.p
+        return self
+    
+def dist(s,o):
+    return np.sqrt((o[0]-s[0])**2+(o[1]-s[1])**2)
+
+# testEnv = Environment(100,200)
+# testEnv.initUAVs(10)
+# for i in range(100):
+#     testEnv.updatePos()
+#     testEnv.updateValue
+# for i in range(100):
+#     testEnv.updatePos()
+#     testEnv.updateVel()
+#     testEnv.updateValue()
+# plt.plot(0,0,'rp')
+# plt.plot(testEnv.target[0],testEnv.target[1],'b*')
+# for u in testEnv.UAVs:
+#     plt.plot(u.x[0],u.x[1],'gd')
+#      #plt.gca().arrow(u.x[0], u.x[1], u.v[0], u.v[1], head_width=0.1, head_length=0.1, fc='g', ec='g')
+# plt.gca().set_xlim(testEnv.xRange[0], testEnv.xRange[1])
+# plt.gca().set_ylim(testEnv.yRange[0], testEnv.yRange[1])
+# plt.gca().grid(True)
+# print('Target at -->',testEnv.target)
+# print('Guess at -->',testEnv.g)
+# plt.show()
--- UAV_Swarm.py
+++ UAV_Swarm.py
@@ -1,3 +1,34 @@
-import UAV_Class as uav
+import UAV_Class as UAV
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 
-print('Test')
+h = 1000 # search area's height
+w = 1500 # search area's width
+numUAVs = 15
+search = UAV.Environment(h,w)
+search.initUAVs(numUAVs)
+time = 500
+
+fig1, ax1 = plt.subplots()
+def animate(step):
+    ax1.clear()
+    ax1.plot(search.target[0],search.target[1],'r*')
+    for uav in search.UAVs:
+        ax1.plot(uav.x[0], uav.x[1], 'bd')
+    if step <= 100:
+        search.updatePos()
+        search.updateValue()
+    else:
+        search.updatePos()
+        search.updateVel()
+        search.updateValue()
+    ax1.set_xlim(search.xRange[0],search.xRange[1])
+    ax1.set_ylim(search.yRange[0],search.yRange[1])
+    ax1.set_title('UAV Positions Over Time')
+    ax1.set_ylabel('Y')
+    ax1.set_xlabel('X')
+    ax1.grid(True)
+
+ani = FuncAnimation(fig1, animate, frames=time, interval=20)
+ani.save('uav_animation.gif', writer='pillow', fps=5)
+
