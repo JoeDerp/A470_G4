1e3e160809c90fb7ad651df65291be3bb0fe37bc
Noah Fabrick <nfabrick157@gmail.com>
2024-03-22 17:41:44 -0700

    finsihed

--- UAV_Swarm.py
+++ UAV_Swarm.py
@@ -1,18 +1,25 @@
 import UAV_Class as UAV
 import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
+import numpy as np
+from matplotlib.animation import *
 
 h = 1000 # search area's height
 w = 1500 # search area's width
 numUAVs = 20
-fov = 20
+fov = 50
 search = UAV.Environment(h,w)
 search.initUAVs(numUAVs,fov)
 time = 1000
 
 fig1, ax1 = plt.subplots()
 ds = []
+foundT = None
 def animate(step):
+    global foundT
+    global ani
+    if foundT == None:
+        if search.targetFound == True:
+            foundT = step
     ax1.clear()
     ax1.plot(search.target[0],search.target[1],'r*')
     for uav in search.UAVs:
@@ -32,7 +39,9 @@ def animate(step):
     ax1.grid(True)
 
 ani = FuncAnimation(fig1, animate, frames=time, interval=1)
-ani.save('uav_animation.gif', writer='pillow', fps=45)
+ani.save('uav_animation.gif', writer='pillow', fps=50)
+
+
 
 fig2, ax2 = plt.subplots()
 for i in range(numUAVs):
@@ -40,8 +49,11 @@ for i in range(numUAVs):
 ax2.set_title('UAV Distances Over Time')
 ax2.set_ylabel('Distance to Target')
 ax2.set_xlabel('Time')
+ax2.set_xlim(0,foundT+100)
 ax2.grid(True)
 
+
+print(f'The target was found to be at {search.g} at time {foundT}, giving a error of {[np.abs(search.g[0]-search.target[0]),np.abs(search.g[1]-search.target[1])]}')
 plt.show()
 
 
--- test.py
+++ test.py
@@ -1,78 +1,33 @@
-import numpy as np
-
-print(np.random.choice([i+1 for i in range(3)]))
-
-# import numpy as np
-# import matplotlib.pyplot as plt
-
-# class UAV:
-#     def __init__(self,x,v,w,phi_p,phi_g):
-#         self.x = x
-#         self.v = v
-#         self.w = w
-#         self.p = [0,0] # initial best postion of UAV is home position
-#         self.phi_p = phi_p
-#         self.phi_g = phi_g
-
-# class Environment:
-# # Environment represents the scanable area where UAVs will search for target
-# # Given height and width, will create a corresponding x and y range centered at the origin
-#     def __init__(self,height,width):
-#         self.h = height
-#         self.w = width
-#         self.xRange = [-(width/2),(width/2)]
-#         self.yRange = [-(height/2),(height/2)]
-#         self.targets = []
-#         self.g = [] # best known position of target(s)
-#         self.UAVs = []
-
-#     def generateTargets(self, numTargets):
-#         for i in range(numTargets):
-#             self.targets.append((np.random.uniform(self.xRange[0],self.xRange[1]),np.random.uniform(self.yRange[0],self.yRange[1])))
-#             self.g.append((np.random.uniform(self.xRange[0],self.xRange[1]),np.random.uniform(self.yRange[0],self.yRange[1])))
-#         return self
-    
-#     def initUAVs(self, numUAVs):
-#         for i in range(numUAVs):
-#             angle = ((2*np.pi)/numUAVs)*i
-#             v_i = [np.cos(angle),np.sin(angle)]
-#             self.UAVs.append(UAV([0,0],v_i,0.8,0.1,0.1))
-#             self.UAVs[i].target_i = np.random.choice([t for t in range(len(self.targets))])
-
-#     def updatePos(self):
-#         for uav in self.UAVs:
-#             for i in uav.x:
-#                 uav.x[i] += uav.v[i]
-#         return self
-
-#     def updateVel(self,g):
-#         for uav in self.UAVs:
-#             rP = np.random.uniform(0,1)
-#             rG = np.random.uniform(0,1)
-#             for i in range(len(uav.v)):
-#                 uav.v[i] = uav.w*uav.v[i] + uav.phi_p*rP*(uav.p[i]-uav.x[i]) + uav.phi_g*rG*(self.g[uav.target_i][i]-uav.x[i]) 
-#         return self
-
-#     def updateValue(self):
-#         if np.sqrt(self.x) < func(self.p):
-#             self.p = self.x
-#         if func(self.p) < func(g):
-#             g = self.p
-#         return self.p,g
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+from matplotlib.patches import Circle
 
 
+# Constants
+fig, ax = plt.subplots()
+ax.set_xlim(-20, 5)
+ax.set_ylim(-1, 20)
+particle = Circle((0, 0), radius=0.1, fc='red')
+ax.add_patch(particle)
 
-# testEnv = Environment(100,200)
-# testEnv.generateTargets(3)
-# testEnv.initUAVs(10)
-# plt.plot(0,0,'rp')
-# for t in testEnv.targets:
-#     plt.plot(t[0],t[1],'b*')
-# for u in testEnv.UAVs:
-#     plt.gca().arrow(u.x[0], u.x[1], u.v[0], u.v[1], head_width=0.1, head_length=0.1, fc='g', ec='g')
-# # plt.gca().set_xlim(testEnv.xRange[0], testEnv.xRange[1])
-# # plt.gca().set_ylim(testEnv.yRange[0], testEnv.yRange[1])
-# plt.gca().set_xlim(-2, 2)
-# plt.gca().set_ylim(-2, 2)
-# plt.gca().grid(True)
-# plt.show()
+total_frames = 100  # Total number of frames for the animation
+stop_at_frame = 50  # Stop the animation at this frame
+
+# Animation function
+def animate(frame):
+    x, y = particle.center
+    print(frame)
+    if frame >= stop_at_frame:  # Stop the animation when the frame reaches stop_at_frame
+        anim.event_source.stop()
+    else:
+        particle.center = (x - 0.1, y)
+    return particle,
+
+# Create animation
+anim = FuncAnimation(fig, animate, frames=total_frames, interval=2, blit=True)
+
+# Save animation as GIF using Pillow writer
+
+anim.save('particle_animation.gif', writer='pillow', fps=20)
+
+plt.show()
